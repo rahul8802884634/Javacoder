36142f6c3778813a839f7e1e4a16b6afd3aa39df
rahul@whiz-solutions.com <rahul@whiz-solutions.com>
2025-06-15 11:34:33 +0530

    code commit

---
+++ src/ex_02_Enum/Lab197_ENUM.java
@@ -0,0 +1,12 @@
+package ex_02_Enum;
+
+public class Lab197_ENUM {
+    public static void main(String[] args) {
+        System.out.println(Day.SUNDAY);
+    }
+}
+
+// Constants list
+enum Day{
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
---
+++ src/ex_24_Exceptions/Lab210.java
@@ -0,0 +1,4 @@
+package ex_24_Exceptions;
+
+public class Lab210 {
+}
